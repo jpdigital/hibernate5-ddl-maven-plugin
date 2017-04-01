ec440d9d3b14bdab92f5b057b8e66ec022042610
Jens Pelzetter <jens@jp-digital.de>
2017-04-01 16:34:07 +0200

    Updated Hibernate to 5.2.9

--- src/main/java/de/jpdigital/maven/plugins/hibernate5ddl/GenerateDdlMojo.java
+++ src/main/java/de/jpdigital/maven/plugins/hibernate5ddl/GenerateDdlMojo.java
@@ -38,7 +38,6 @@ import org.xml.sax.helpers.DefaultHandler;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
