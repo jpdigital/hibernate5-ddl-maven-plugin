279e76c70bb9f96dbd087126add59a15706b4d5b
Steven Li <steven.li2@spark.co.nz>
2023-06-30 15:06:55 +1200

    Added support for Hibernate 6.0 and 6.1

---
+++ hibernate61-ddl-maven-plugin/src/main/java/de/jpdigital/maven/plugins/hibernate5ddl/Hibernate61GenerateDdlMojo.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2017 Jens Pelzetter
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.jpdigital.maven.plugins.hibernate5ddl;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+/**
+ * Provides a goal which creates DDL SQL files for the JPA entities in the
+ * project (using the Hibernate 5 SchemaExport class}.
+ * 
+ * The actual logic is in {@link GenerateDdlMojo}, but without this class Maven
+ * does not find the goal.
+ *
+ * @author <a href="mailto:jens.pelzetter@googlemail.com">Jens Pelzetter</a>
+ */
+@Mojo(name = "gen-ddl",
+      defaultPhase = LifecyclePhase.PROCESS_CLASSES,
+      requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME,
+      threadSafe = true)
+public class Hibernate61GenerateDdlMojo extends GenerateDdlMojo {
+
+}
